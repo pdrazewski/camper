112d97be0142e3c628f44911dc1f7a2134877f59
defehub <drazewski.przemyslaw@gmail.com>
2016-07-27 22:13:21 +0200

    Remove server

--- server.js
+++
@@ -1,6 +0,0 @@
-// app.js 
-var express = require('express');
-var app = module.exports.app = exports.app = express();
- 
-//you won't need 'connect-livereload' if you have livereload plugin for your browser 
-app.use(require('connect-livereload')());

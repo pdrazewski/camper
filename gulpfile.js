20575de2044bc3f343290b3e6a7d0628ea4a49a8
defehub <drazewski.przemyslaw@gmail.com>
2016-07-27 06:24:42 +0200

    Add sass, multiimport

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp');
 var nunjucksRender = require('gulp-nunjucks-render');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-
+var bulkSass = require('gulp-sass-bulk-import');
+var clean = require('gulp-clean');
 
 // compile htmls
 gulp.task('compile', function() {
@@ -48,20 +49,48 @@ gulp.task('sass', function () {
 	    .resume()
 });
 
+
+gulp.task('sass-multi-import', function() {
+	var appname = false;
+ 	i = process.argv.indexOf("--app");
+ 	if(i>-1) {appname = process.argv[i+1]}
+ 	var dest = appname ? 'apps/'+appname+'/_dist/common/css/' : 'camper/_dist/common/css/';
+ 	var input;
+ 	if (appname) {
+ 		input = ['apps/'+appname+'/_src/_common/css/assets.scss']
+ 		output = 'apps/'+appname+'/_src/_common/css/_imports.scss'
+ 	} else {
+ 		input = ['camper/_src/_common/css/assets.scss']
+ 		output = 'camper/_src/_common/css/_imports.scss'
+ 	}
+ 	console.log(input, output)
+   return gulp
+    .src(input)
+    .pipe(bulkSass())
+    .pipe(clean(output))
+    .pipe(gulp.dest(output) );
+});
+
+
 // watch for everything
 gulp.task('watch', function() {
 	var appname = false;
  	i = process.argv.indexOf("--app");
  	if(i>-1) {appname = process.argv[i+1]}
-	var input = [
+	var inputHTML = [
 	'camper/_src/pages/**/*.+(html|nunjucks)',
 	'apps/'+appname+'/_src/pages/**/*.+(html|nunjucks)'
 	]
-  	return gulp
-    	.watch(input, ['compile'])
+	if (appname) {
+ 		inputCSS = ['apps/'+appname+'/_src/_common/css/**/*.scss','apps/'+appname+'/_src/_common/css/*.scss']
+ 	} else {
+ 		inputCSS = ['camper/_src/_common/css/**/*.scss','camper/_src/_common/css/*.scss']
+ 	}
+  	gulp.watch(inputHTML, ['compile']);
+    gulp.watch(inputCSS, ['sass-multi-import','sass']);
 });
 
-//scss
+
 //js hint
 //react-babel builds
 //express server
